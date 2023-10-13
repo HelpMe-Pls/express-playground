c8da6121d3b3d21b01b5725561d72e5daa46a1ed
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2023-10-13 15:13:59 +0700

    done

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Redirect to corresponding handler in the `routes` folder
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/catalog", catalogRouter);
